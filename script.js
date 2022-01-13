a1e17cf30a816de9df5651093a0669da209f5efe
ASHUTOSH <mukutrai12@gmail.com>
2022-01-14 00:55:13 +0530

    firstcommit

---
+++ script.js
@@ -0,0 +1,18 @@
+var toggle = false;
+var mybuttn = document.getElementById("button");
+var mypara = document.getElementById("para");
+mybuttn.addEventListener("click", function () {
+  if (!toggle) {
+    document.body.style.backgroundColor = "black";
+    toggle = true;
+    document.getElementById("para").style.color = "white";
+    mybuttn.style.marginLeft = "100px";
+  } else {
+    document.body.style.background = "white";
+    toggle = false;
+    document.getElementById("para").style.color = "black";
+    mybuttn.style.marginLeft = "10px";
+  }
+});
+//mypara.addEventListener("click", function()({
+//document.body.style.color="white";//
